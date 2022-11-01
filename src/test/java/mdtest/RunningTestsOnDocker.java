daa3ec38599cf61044b3603175f4c03e3cf60339
Abdulla Suhail <abdulla.suhail8@gmail.com>
2022-11-01 18:58:49 +0530

    first commit

---
+++ src/test/java/mdtest/RunningTestsOnDocker.java
@@ -0,0 +1,30 @@
+package mdtest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class RunningTestsOnDocker {
+
+	@Test
+	public void runtestsondocker() {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("http://localhost:8070/webapp/");
+		String text = driver.findElement(By.xpath("(//h2)[2]")).getText();
+		Assert.assertEquals("Good Luck and we are almost done with the course.Thank you", text);
+		driver.close();
+
+	}
+
+}
